6dacfb6ba69d362e3c8a1bd4eaa55efb77f730e9
J K <kj688895@gmail.com>
2020-09-18 16:34:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,15 @@
 def prime?(num)
   n = 2
+  remainders = []
   while n < num
     return false if num % n == 0
     n += 1
   end
-  true
-end
+  if num > 1 then remainders.all? {|remainder| remainder != 0}
+  else
+    false
+  end
+end 
 
 prime?(3)
 
